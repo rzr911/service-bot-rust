c3a92e56641efaa9acefc0dc50c2101d5e310212
sree <rajan.sreeraj@yahoo.com>
2020-10-11 15:46:11 +0530

    remove sudo

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
-sudo apt-get update -y;
-sudo apt-get install -y \
+apt-get update -y;
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common;
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
+apt-get install -y docker-ce docker-ce-cli containerd.io;
 
-sudo groupadd docker;
+groupadd docker;
 
-sudo usermod -aG docker $USER;
+usermod -aG docker $USER;
 newgrp docker;
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-sudo chmod +x /usr/local/bin/docker-compose;
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
 
 
